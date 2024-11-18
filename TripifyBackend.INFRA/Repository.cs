77ae83ad482caf0014f08dc247a3c34c2764c3a9
João Vieira <56136565+j0a0vieira@users.noreply.github.com>
2024-11-18 16:06:30 +0000

    Modificação para não inserir valores duplicados

--- TripifyBackend.INFRA/Repository.cs
+++ TripifyBackend.INFRA/Repository.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using TripifyBackend.DOMAIN.Interfaces.Repository;
 using TripifyBackend.DOMAIN.Models;
 using TripifyBackend.INFRA.DBContext;
@@ -19,14 +20,27 @@ public class Repository : IRepository
     
     public async Task<List<PlaceDomain>> FillDatabase(List<PlaceDomain> places)
     {
+        var addedPlaces = new List<PlaceDB>();
         foreach (var placeDomain in places)
         {
             var db = _mapper.Map<PlaceDB>(placeDomain);
-            await _context.Places.AddAsync(db);
+            if (await VerifyDuplicatePlace(db) == false)
+            {
+                await _context.Places.AddAsync(db);
+                addedPlaces.Add(db);
+            }
         }
 
         
         await _context.SaveChangesAsync();
         return places;
+    }
+
+    private async Task<bool> VerifyDuplicatePlace(PlaceDB place)
+    {
+        var placeFind = await _context.Places
+            .FirstOrDefaultAsync(p => p.Name.Equals(place.Name));
+        
+        return placeFind != null ? true : false;
     }
 }
